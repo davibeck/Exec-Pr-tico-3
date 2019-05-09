f52e90528a5f0023ff78b9139b249ed4392951c6
Bredstone <48719973+Bredstone@users.noreply.github.com>
2019-05-09 11:20:16 -0300

    Add files via upload

--- Calculadora.java
+++ Calculadora.java
@@ -1,42 +1,62 @@
-package Package;
+package GUI;
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
 
 public class Calculadora extends JFrame implements ActionListener {
 	private GridBagLayout layout; // layout of this frame
 	private GridBagConstraints constraints; // constraints of this layout
-	boolean c = false;
-	int v1 = 0, v2 = 0;
-	String op;
+	boolean c = true;
+	double x = ' ';
 
 	public Calculadora() {
 		super("Calculadora");
 		layout = new GridBagLayout();
 		setLayout(layout); // set frame layout
 		constraints = new GridBagConstraints(); // instantiate constraints
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		ScriptEngine engine = mgr.getEngineByName("JavaScript");
 
-		
 		JTextField textField = new JTextField("");
-		JButton button1 = new JButton("1");
-		JButton button2 = new JButton("2");
-		JButton button3 = new JButton("3");
-		JButton button4 = new JButton("4");
-		JButton button5 = new JButton("5");
-		JButton button6 = new JButton("6");
-		JButton button7 = new JButton("7");
-		JButton button8 = new JButton("8");
-		JButton button9 = new JButton("9");
-		JButton button0 = new JButton("0");
-		JButton buttonDiv = new JButton("/");
-		JButton buttonMul = new JButton("*");
-		JButton buttonSub = new JButton("-");
-		JButton buttonSom = new JButton("+");
+		textField.setEditable(false);
+		JButton[] numberButtons = new JButton[10];
+
+		for (int i = 0; i < numberButtons.length; i++) {
+			numberButtons[i] = new JButton(i + "");
+			numberButtons[i].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent a) {
+					if (c)
+						textField.setText("");
+					textField.setText(textField.getText() + a.getActionCommand());
+					c = false;
+				}
+			});
+		}
+		
+		JButton[] basicButtons = new JButton[4];
+
+		basicButtons[0] = new JButton("/");
+		basicButtons[1] = new JButton("*");
+		basicButtons[2] = new JButton("-");
+		basicButtons[3] = new JButton("+");
+		
+		for (int i = 0; i < basicButtons.length; i++) {
+			basicButtons[i].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent a) {
+					c = false;
+					textField.setText(textField.getText() + " " + a.getActionCommand() + " ");
+				}
+			});
+		}
+		
 		JButton buttonIg = new JButton("=");
 		JButton buttonPon = new JButton(".");
 		JButton buttonx = new JButton("x!");
-		JButton buttonln = new JButton("e^x");
+		JButton buttonExpo = new JButton("e^x");
 		JButton buttonRaiz = new JButton("raiz 2");
 		JButton buttonRaizc = new JButton("raiz 3");
 		JButton buttonPor = new JButton("%");
@@ -47,403 +67,209 @@ public class Calculadora extends JFrame implements ActionListener {
 		JButton clear = new JButton("C");
 		JButton buttonPD = new JButton("(");
 		JButton buttonPE = new JButton(")");
-		
-		
-		
-	
-		
-		button1.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	
-            	if(!c) {
-            		v1 = 1;
-            		c = true;
-            	}else {
-            		v2 = 1;
-            	}
-            }
-        });
-		
-		button2.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	if(!c) {
-            		v1 = 2;
-            		c = true;
-            	}else {
-            		v2 = 2;
-            	}
-            }
-        });
-		
-		button3.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	if(!c) {
-            		v1 = 3;
-            		c = true;
-            	}else {
-            		v2 = 3;
-            	}
-            }
-        });
-		
-		button4.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	if(!c) {
-            		v1 = 4;
-            		c = true;
-            	}else {
-            		v2 = 4;
-            	}
-            }
-        });
-		
-		button5.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	if(!c) {
-            		v1 = 5;
-            		c = true;
-            	}else {
-            		v2 = 5;
-            	}
-            }
-        });
-		
-		button6.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	if(!c) {
-            		v1 = 6;
-            		c = true;
-            	}else {
-            		v2 = 6;
-            	}
-            }
-        });
-		
-		button7.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	if(!c) {
-            		v1 = 7;
-            		c = true;
-            	}else {
-            		v2 = 7;
-            	}
-            }
-        });
-      
-		button8.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	if(!c) {
-            		v1 = 8;
-            		c = true;
-            	}else {
-            		v2 = 8;
-            	}
-            }
-        });
-		   
-		button9.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	if(!c) {
-            		v1 = 9;
-            		c = true;
-            	}else {
-            		v2 = 9;
-            	}
-            }
-        });
-		
-		button0.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	if(!c) {
-            		v1 = 0;
-            		c = true;
-            	}else {
-            		v2 = 0;
-            	}
-            }
-        });
-		
-		buttonDiv.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonMul.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonSub.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonSom.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonIg.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonPon.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonx.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonln.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonRaiz.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		
-		buttonRaizc.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonPor.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonQua.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonCub.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonxy.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		buttonPi.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	op = "/";
-            }
-        });
-		
-		clear.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	
-            }
-        });
-		
-		buttonPD.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	
-            }
-        });
-		
-		buttonPE.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent a)
-            {
-            	
-            }
-        });
-		
-		   constraints.fill = GridBagConstraints.BOTH;
-		   constraints.weightx = 1;
-		   constraints.weighty = 4; 
-		   addComponent( textField, 0, 0, 7, 1);
-		   		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1; 
-		   addComponent( button1, 1, 0, 1, 1 );      
 
-		   constraints.weightx = 1;  
-		   constraints.weighty = 1;     
-		   addComponent( button2, 1, 1, 1, 1 );
+		buttonPon.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				c = false;
+				textField.setText(textField.getText() + a.getActionCommand());
+			}
+		});
 
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( button3, 1, 2, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonDiv, 1, 3, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonx, 1, 4, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonPor, 1, 5, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonPi, 1, 6, 1, 1 );
+		buttonx.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				try {
+					double resp = Double.parseDouble(engine.eval(textField.getText()).toString());
+					textField.setText(fat(resp) + "");
+				} catch (ScriptException e) {
+					textField.setText("ERRO!");
+				}
 
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( button4, 2, 0, 1, 1 );
-		   
-		   constraints.weightx = 1;  
-		   constraints.weighty = 1;     
-		   addComponent( button5, 2, 1, 1, 1 );
+				c = true;
+				;
+			}
+		});
 
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( button6, 2, 2, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonMul, 2, 3, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonln, 2, 4, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonQua, 2, 5, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( clear, 2, 6, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( button7, 3, 0, 1, 1 );
-		   
-		   constraints.weightx = 1;  
-		   constraints.weighty = 1;     
-		   addComponent( button8, 3, 1, 1, 1 );
+		buttonExpo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				try {
+					double resp = Double.parseDouble(engine.eval(textField.getText()).toString());
+					textField.setText(Math.exp(resp) + "");
+				} catch (ScriptException e) {
+					textField.setText("ERRO!");
+				}
 
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( button9, 3, 2, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonSub, 3, 3, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonRaiz, 3, 4, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonCub, 3, 5, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonPD, 3, 6, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( button0, 4, 0, 1, 1 );
-		   
-		   constraints.weightx = 1;  
-		   constraints.weighty = 1;     
-		   addComponent( buttonPon, 4, 1, 1, 1 );
+				c = true;
+			}
+		});
 
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonIg, 4, 2, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonSom, 4, 3, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonRaizc, 4, 4, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonxy, 4, 5, 1, 1 );
-		   
-		   constraints.weightx = 1;
-		   constraints.weighty = 1;    
-		   addComponent( buttonPE, 4, 6, 1, 1 );
+		buttonRaiz.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				try {
+					double resp = Double.parseDouble(engine.eval(textField.getText()).toString());
+					textField.setText(Math.sqrt(resp) + "");
+				} catch (ScriptException e) {
+					textField.setText("ERRO!");
+				}
+
+				c = true;
+			}
+		});
+
+		buttonRaizc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				try {
+					double resp = Double.parseDouble(engine.eval(textField.getText()).toString());
+					textField.setText(Math.cbrt(resp) + "");
+				} catch (ScriptException e) {
+					textField.setText("ERRO!");
+				}
+
+				c = true;
+			}
+		});
+
+		buttonPor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				try {
+					double resp = Double.parseDouble(engine.eval(textField.getText()).toString());
+					textField.setText(resp / 100 + "");
+				} catch (ScriptException e) {
+					textField.setText("ERRO!");
+				}
+
+				c = true;
+			}
+		});
+
+		buttonQua.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				try {
+					double resp = Double.parseDouble(engine.eval(textField.getText()).toString());
+					textField.setText(Math.pow(resp, 2) + "");
+				} catch (ScriptException e) {
+					textField.setText("ERRO!");
+				}
+
+				c = true;
+			}
+		});
+
+		buttonCub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				try {
+					double resp = Double.parseDouble(engine.eval(textField.getText()).toString());
+					textField.setText(Math.pow(resp, 3) + "");
+				} catch (ScriptException e) {
+					textField.setText("ERRO!");
+				}
+
+				c = true;
+			}
+		});
+
+		buttonxy.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				try {
+					double resp = Double.parseDouble(engine.eval(textField.getText()).toString());
+					x = resp;
+				} catch (ScriptException e) {
+					textField.setText("ERRO!");
+				}
+
+				c = true;
+			}
+		});
+
+		buttonPi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				if (c)
+					textField.setText("");
+				textField.setText(textField.getText() + Math.PI);
+			}
+		});
+
+		clear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				textField.setText("");
+			}
+		});
+
+		buttonPD.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				if (c)
+					textField.setText("");
+				textField.setText(textField.getText() + a.getActionCommand());
+			}
+		});
+
+		buttonPE.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				if (c)
+					textField.setText("");
+				textField.setText(textField.getText() + a.getActionCommand());
+			}
+		});
+
+		buttonIg.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				try {
+					if (x == ' ')
+						textField.setText(engine.eval(textField.getText()).toString());
+					else {
+						double resp = Double.parseDouble(engine.eval(textField.getText()).toString());
+						textField.setText(Math.pow(x, resp) + "");
+						x = ' ';
+					}
+				} catch (ScriptException e) {
+					textField.setText("ERRO!");
+				}
+
+				c = true;
+			}
+		});
+
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 1;
+		constraints.weighty = 4;
+		addComponent(textField, 0, 0, 7, 1);
+
+		constraints.weightx = 1;
+		constraints.weighty = 1;
+		addComponent(numberButtons[1], 1, 0, 1, 1);
+		addComponent(numberButtons[2], 1, 1, 1, 1);
+		addComponent(numberButtons[3], 1, 2, 1, 1);
+
+		addComponent(basicButtons[0], 1, 3, 1, 1);
+		addComponent(buttonx, 1, 4, 1, 1);
+		addComponent(buttonPor, 1, 5, 1, 1);
+		addComponent(buttonPi, 1, 6, 1, 1);
+
+		addComponent(numberButtons[4], 2, 0, 1, 1);
+		addComponent(numberButtons[5], 2, 1, 1, 1);
+		addComponent(numberButtons[6], 2, 2, 1, 1);
+
+		addComponent(basicButtons[1], 2, 3, 1, 1);
+		addComponent(buttonExpo, 2, 4, 1, 1);
+		addComponent(buttonQua, 2, 5, 1, 1);
+		addComponent(clear, 2, 6, 1, 1);
+
+		addComponent(numberButtons[7], 3, 0, 1, 1);
+		addComponent(numberButtons[8], 3, 1, 1, 1);
+		addComponent(numberButtons[9], 3, 2, 1, 1);
+
+		addComponent(basicButtons[2], 3, 3, 1, 1);
+		addComponent(buttonRaiz, 3, 4, 1, 1);
+		addComponent(buttonCub, 3, 5, 1, 1);
+		addComponent(buttonPD, 3, 6, 1, 1);
+
+		addComponent(numberButtons[0], 4, 0, 1, 1);
+
+		addComponent(buttonPon, 4, 1, 1, 1);
+		addComponent(buttonIg, 4, 2, 1, 1);
+		addComponent(basicButtons[3], 4, 3, 1, 1);
+		addComponent(buttonRaizc, 4, 4, 1, 1);
+		addComponent(buttonxy, 4, 5, 1, 1);
+		addComponent(buttonPE, 4, 6, 1, 1);
 
 	}
 
@@ -461,12 +287,22 @@ public class Calculadora extends JFrame implements ActionListener {
 		// TODO Auto-generated method stub
 
 	}
-	
+
+	public static double fat(double n) {
+		double fat = 1;
+
+		for (double i = n; i > 1; i--) {
+			fat *= i;
+		}
+
+		return fat;
+	}
+
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
 		calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		calc.setSize(400, 250); 
-		calc.setVisible(true); 
+		calc.setSize(400, 250);
+		calc.setResizable(false);
+		calc.setVisible(true);
 	}
-
 }
